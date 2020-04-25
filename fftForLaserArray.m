f9fafdfce379d04ad05ac3cd1bb5865e78c77ead
minspan1999 <minspan@users.noreply.github.com>
2020-04-25 11:49:20 -0400

    fftForLaserArray debugged

--- fftForLaserArray.m
+++ fftForLaserArray.m
@@ -3,10 +3,11 @@ clc;
 clear all;
 close all;
 
-radiumOfPixel = 5e-6;
+radiumOfPixel = 10e-6;
+spotSize = 5e-6;
 xNum = 6;
 yNum = 6;
-N = 3000; %number of pixels in each dimension (determines fidelity and processing time)
+N = 2500; %number of pixels in each dimension (determines fidelity and processing time)
 
 
 % Amp = zeros(xNum,yNum);
@@ -25,21 +26,21 @@ AmpPhase = [1.570796327	3.141592654	-1.570796327	-4.21E-15	1.570796327	3.1415926
 2.15E-16	-1.570796327	3.141592654	1.570796327	-2.92E-15	-1.570796327
 ];
 
-dx = 50e-9; %width of each pixel
+dx = 300e-9; %width of each pixel ???
 xN = xNum*radiumOfPixel*2;
 yN = yNum*radiumOfPixel*2;
 screenSize = N*dx;
 
 
-[xs ys] = meshgrid((-N / 2:N / 2 - 1) .* dx + xN/2); % defining near field grid
+[xsNear, ysNear] = meshgrid((-N / 2:N / 2 - 1) .* dx); % defining near field grid + xN/2
 lambda = 1550e-9;
 k = 2*pi/lambda;
-z = 100;
+z = 10;
 
 % grid_size = N * dx; % number of pixels in one dimension times pixel width
 % [xs ys] = meshgrid((-xN / 2:xN / 2 - 1)); % defining near field grid
 dxFar = lambda * z / screenSize; % far field pixel width
-[x_det y_det] = meshgrid((-N / 2:N / 2 - 1) .* dxFar + yN/2); % defining far field grid
+[xdFar, ydFar] = meshgrid((-N / 2:N / 2 - 1) .* dxFar); % defining far field grid + yN/2
 
 nearField = zeros(N,N);
 nearFields{xNum, yNum} = nearField;
@@ -51,8 +52,8 @@ N_LOOP = xNum*yNum;
 parfor loopNum = 1:N_LOOP
 
     [ii, jj] = getInd(loopNum-1, xNum);
-    nearFields{loopNum} =  Amp(ii, jj) * exp((-((xs - (2*jj - 1) * radiumOfPixel).^2 ...
-            + (ys - (2*ii - 1) * radiumOfPixel).^2) / (radiumOfPixel^2))) * exp(1i * AmpPhase(ii, jj));
+    nearFields{loopNum} =  Amp(ii, jj) * exp((-((xsNear - (jj - 1) * radiumOfPixel).^2 ...
+            + (ysNear - (ii - 1) * radiumOfPixel).^2) / (spotSize^2))) * exp(1i * AmpPhase(ii, jj));
     nearField = nearField + nearFields{loopNum};
 end
 
@@ -66,16 +67,16 @@ parfor loopNum = 1:N_LOOP
    [ii, jj] = getInd(loopNum-1, xNum);
 
        Angular_Spectrum = fftshift(fft2(fftshift(nearFields{loopNum}))); %Near Field
-       farFields{loopNum} = exp(1i*k*z)*exp(1i*k*(xs.^2+ys.^2)/(2*z)).*Angular_Spectrum/(1i*lambda*z);
+       farFields{loopNum} = exp(1i*k*z)*exp(1i*k*(xdFar.^2+ydFar.^2)/(2*z)).*Angular_Spectrum/(1i*lambda*z);
        farField = farField + farFields{loopNum};
    
 end
 
-theta_sim = 20;
+theta_sim = 10;
 
 % PLOTTING FAR FIELDS
-angle_x = atan(x_det / z) * 360 / (2 * pi); %translating to angular values
-angle_y = atan(y_det / z) * 360 / (2 * pi);
+angle_x = atan(xdFar / z) * 360 / (2 * pi); %translating to angular values
+angle_y = atan(ydFar / z) * 360 / (2 * pi);
 
 angleX_Min = min(min(angle_x)); %translating to angular values
 angleX_Max = max(max(angle_x));
@@ -102,6 +103,14 @@ figure;
 imagesc(angleX_Min, -angleY_Min, angle(farFieldNormalizedIntensity));
 axis([-theta_sim theta_sim -theta_sim theta_sim]);
 
+AmpIFFT = fftshift(ifft2(fftshift(farField))); %Near Field
+bearFieldsIFFT = exp(1i*k*z)*exp(1i*k*(xsNear.^2+ysNear.^2)/(2*z)).*AmpIFFT/(1i*lambda*z);
+
+figure;
+imagesc(abs(AmpIFFT))
+
+figure;
+imagesc(angle(AmpIFFT))
 
 
 function [ind1, ind2] = getInd(loopNum, size)
